7be144e448b5f633da68752a54e62b46ac25b8f4
Sharif03 <abinsu7@gmail.com>
2018-09-28 20:44:03 +0600

    Just simple multipliction programme

---
+++ Multiplication.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a=5; b=10; result;
+    result=a*b; 
+
+    printf("THe Multiplication result is: %d", result); //Do Multiplication and print this.
+    return 0;
+}
